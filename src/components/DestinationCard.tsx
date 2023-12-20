8815d6cc95b8ea73c63bae9ef6ad081d5f19adbb
aspark-dipesh <dipesh.chaulagain@asparksys.com>
2023-12-20 16:13:04 +0545

    Ready for muck view

--- src/components/DestinationCard.tsx
+++ src/components/DestinationCard.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react'
 interface Props {
     title?: string;
@@ -9,7 +10,7 @@ const DestinationCard = ({ title, image, link }: Props) => {
         <div>
             <div className="card w-full bg-base-100 shadow-xl">
                 <div className='aspect-[8/7] relative'>
-                    <img src={image!} alt="hero" className="h-full w-full object-cover z-10 rounded-lg" />
+                    <Image fill src={image!} alt="hero" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" className="object-cover z-10 rounded-lg" />
                 </div>
                 <div className="card-body p-2 h-fit">
                     <h2 className="card-title text-base">{title}</h2>
